fdda31eaefdf029826c0b3571c088a60d1ebbd2c
Raghu Paredla <raghuram5910@gmail.com>
2019-10-02 07:09:43 +0530

    commit

---
+++ brew.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install google-backup-and-sync
+brew cask install skype
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
+
