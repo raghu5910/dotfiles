70f7ed590477379e41816acd482d3718d594fe92
Raghu Paredla <raghuram5910@gmail.com>
2019-10-09 09:35:57 +0530

    automating settings configuration

---
+++ settings/installSettings.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: install.sh <home_directory>"
+    exit 1
+fi
+
+homedir=$1
+
+# dotfiles settings directory
+settingsdir = ${homedir}/dotfiles/settings
+
+# Create symlink for vscode user settings
+echo "Creating symlink to settings.json in /Library/Application Support/Code/User/"
+ln -sf $settingsdir/VSCode-Settings.json $homedir/Library/Application\ Support/Code/User/settings.json
