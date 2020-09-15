c502d267a0babb1ed104a9402e388627c6873c40
raghu5910 <raghuram5910@gmail.com>
2020-09-15 20:26:53 +0530

    enumerate as 01 not 1

--- file-namer.sh
+++ file-namer.sh
@@ -1,10 +1,27 @@
 #! /usr/bin/env bash
-files=(*)
-num=1
-for file in "${files[@]}"
+files=("$1/*")
+for file in $files
 do
-	num1="${num}.png"
-	mv "$file" "${num1}"
-	num=$(echo "${num}+1" | bc)
-	num1=""
+	echo "$file"
 done
+num=$2
+echo "Continue: y/n ?"
+read response
+if [ "$response" == "y" ]
+then
+	for file in $files
+	do
+		if [[ $num -le 9 ]]
+		then
+			num1="$1/0${num}.png"
+		else
+			num1="$1/${num}.png"
+		fi
+		mv "$file" "${num1}"
+		num=$(echo "${num}+1" | bc)
+		num1=""
+	done
+else
+	echo "Exiting"
+fi
+		
